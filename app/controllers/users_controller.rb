f18ae12dbdfb47798170c84a542382326a1d7556
Chiara Davies <chiaradavies01@outlook.com>
2017-09-21 12:26:14 +0100

    positioned the devise sign in form centrally

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products, only: [:index, :show]
-
+  # resources :users, only: [:show, :edit, :update, :destroy]
 end
