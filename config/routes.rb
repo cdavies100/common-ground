81020151dedd6b3bed441e6f244ea84adecba791
Chiara Davies <chiaradavies01@outlook.com>
2017-10-11 14:01:29 +0100

    routes, styling, basic content for leggings

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'lookbook', to: 'pages#lookbook'
+  get 'leggings', to: 'products#leggings'
+  get 'active_top', to: 'products#top'
+  get 'sports_bra', to: 'products#bra'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products, only: [:index, :show]
   # resources :users, only: [:show, :edit, :update, :destroy]
